3aa0c0a04648a161daa50436bf07dd9ae33547f5
Requero <51053764+Requero@users.noreply.github.com>
2021-03-18 10:27:54 +0100

    Release with global variables

---
+++ kmeans_with_global_variables.py
@@ -0,0 +1,327 @@
+import time
+import random
+import numpy as np
+import lithops.multiprocessing as mplths
+import multiprocessing as mp
+import itertools
+
+lock = None
+barrier = None
+global_centroids = None
+global_delta = None
+worker_stats = None
+
+def main(datapoints_per_file, dimensions,clusters,parallelism,number_of_iterations,threshold,local, localhost= False):
+
+    manager = mp.Manager() if local == True else mplths.Manager()
+    global lock
+    lock = manager.Lock()
+    global barrier
+    barrier = manager.Barrier(parallelism)
+    global global_centroids
+    global_centroids = manager.dict()
+    global global_delta
+    global_delta = manager.dict({"delta": 0.0, "delta_c": 0.0, 
+                                "delta_temp": 0.0, "delta_st": 0.0})
+    global worker_stats
+    worker_stats = manager.list()     # in seconds
+    
+    
+    if True == False:
+        # TEST Crentroids
+        centroids = GlobalCentroids(2, 2)
+        centroids.random_init(4)
+        print(centroids.get_centroids())
+        centroids.update(np.array([[1.2, 1, 1, 1], [2, 2, 2, 2]]), [2, 2])
+        centroids.update(np.array([[2, 2, 2, 2.2], [1, 1, 1, 1]]), [2, 2])
+        print(centroids.get_centroids())
+        # TEST Delta
+        delta = GlobalDelta(2)
+        delta.init()
+        print(delta.get_delta())
+        delta.update(1, 2)
+        delta.update(0, 2)
+        print(delta.get_delta())
+        return
+
+    # Initialize global objects
+    centroids = GlobalCentroids(clusters, parallelism)
+    centroids.random_init(dimensions)
+    delta = GlobalDelta(parallelism)
+    delta.init()
+
+    start_time = time.time()
+    iterator = list(itertools.product(range(parallelism),
+    [datapoints_per_file],[dimensions], [clusters], 
+    [parallelism], [number_of_iterations],[threshold]))
+
+    if local==True:
+        pool = mp.Pool(processes=parallelism)
+        pool.imap(run, iterator)
+        pool.close()
+        pool.join()
+    else:
+        if localhost == True:
+            pool = mplths.Pool(processes=parallelism,initargs = ('localhost'))
+            pool.imap(run, iterator)
+            pool.close()
+            pool.join()
+            
+        else:
+            pool = mplths.Pool(processes=parallelism, initargs=('serverless'))
+            pool.imap(run, iterator)
+            pool.close()
+            pool.join()
+    # Parse results
+    times = []
+    for b in worker_stats:
+        # Iterations time is second breakdown and last
+        times.append(b[-1]-b[2])
+    
+    avg_time = sum(times) / len(times)
+    print(f"Total k-means time: {time.time() - start_time} s")
+    print(f"Average iterations time: {avg_time} s")
+    print(global_centroids.items())
+    print(global_centroids.items())
+    print(global_centroids.items())
+    print(global_centroids.items())
+    where = ''
+    if local ==True:
+        where = 'local'
+    elif localhost == True:
+        where ='localhost'
+    else:
+        where = 'serverless'
+
+    with open('time_break_'+where+'_'+str(datapoints_per_file)+'_'+str(dimensions)+'_'+str(parallelism)+'_'+str(clusters)+'_'+str(number_of_iterations)+'.txt', 'a+') as f:
+        f.write(f"{time.time() - start_time}\n")
+        f.write(f"{avg_time}\n")
+        for item in worker_stats:
+            f.write(f"{item}\n")
+        
+def run(arguments):
+    global lock
+    w_id, points_per_file, dimensions, clusters, parallelism, number_of_iterations,threshold = arguments
+    worker = Worker( w_id, parallelism * points_per_file, dimensions, parallelism,clusters, number_of_iterations, threshold)
+    worker_breakdown = worker.run()
+    lock.acquire()
+    worker_stats.append(worker_breakdown)
+    lock.release()
+
+class GlobalCentroids(object):
+    def __init__(self,clusters, parallelism):
+        self.num_clusters = clusters
+        self.parallelism = parallelism
+
+    @staticmethod
+    def centroid_key(centroid):
+        return "centroid" + str(centroid)
+
+    def random_init(self,num_dimensions):
+        global lock 
+        lock.acquire()
+        for i in range(self.num_clusters):
+            random.seed(1002+i)
+            temp_dict =  {}
+            temp_dict['centroids'] = np.random.normal(0, 1,num_dimensions)
+            temp_dict['counters'] = 0
+            global global_centroids
+            global_centroids[self.centroid_key(i)] = temp_dict
+        lock.release()
+
+    def update(self,coordinates, sizes):
+        for k in range(self.num_clusters):
+            self._update_centroid(k, coordinates[k].tolist(), int(sizes[k]))
+
+    def _update_centroid(self, cluster_id, coordinates, size):
+        # Current centroid
+        global lock
+        centroid_k = self.centroid_key(cluster_id)
+        lock.acquire()
+        global global_centroids
+        temp_dict = global_centroids[centroid_k]
+        n = len(temp_dict['centroids'])
+        count = temp_dict['counters']
+
+        # First update
+        if count == 0:
+            centroid_temp = np.zeros(n)
+            sizes_temp =  0
+        else:
+            centroid_temp = temp_dict['centroids_temp']
+            sizes_temp =temp_dict['sizes_temp']
+
+        centroid_temp =  centroid_temp + coordinates
+        sizes_temp += size
+        count += 1
+        temp_dict['centroids_temp'] = centroid_temp
+        temp_dict['sizes_temp'] = sizes_temp
+        
+        if count== self.parallelism :
+            if sizes_temp != 0:
+                temp_dict['centroids'] = centroid_temp/sizes_temp
+            temp_dict['counters'] = 0
+        else: 
+            temp_dict['counters'] = count
+        global_centroids[centroid_k] = temp_dict
+        lock.release()
+
+    def get_centroids(self):
+        global lock
+        lock.acquire()
+        global global_centroids
+        b = [global_centroids[self.centroid_key(k)]['centroids'] for k in range(self.num_clusters)]
+        lock.release()
+        return b
+
+class GlobalDelta(object):
+    def __init__(self, parallelism):
+        self.parallelism = parallelism
+
+    def init(self):
+        # Hardcoded keys
+        global lock
+        
+        lock.acquire()
+        global global_delta
+        temp_dict = global_delta
+        temp_dict["delta"] = 1
+        temp_dict["delta_c"] = 0
+        temp_dict["delta_temp"] = 0
+        temp_dict["delta_st"] = 0
+        global_delta = temp_dict
+        lock.release()
+
+    def get_delta(self):
+        global lock
+        global global_delta
+        lock.acquire()
+        delta = global_delta["delta"]
+        lock.release()
+        return delta
+
+    def update(self,delta, num_points): 
+        global lock
+        global global_delta
+        lock.acquire()
+        temp_dict = global_delta
+
+        count = temp_dict["delta_c"]
+        tmp_delta = temp_dict["delta_temp"]
+        tmp_points = temp_dict["delta_st"]
+        
+        count += 1
+        tmp_delta += delta
+        tmp_points += num_points
+
+        if count == self.parallelism:
+            temp_dict["delta"] = tmp_delta / tmp_points
+            temp_dict["delta_c"] = 0
+            temp_dict["delta_temp"] = 0
+            temp_dict["delta_st"] = 0
+        else:
+            temp_dict["delta_c"] = count
+            temp_dict["delta_temp"] = tmp_delta
+            temp_dict["delta_st"] = tmp_points
+        global_delta = temp_dict
+        lock.release()
+
+class Worker(object):
+
+    def __init__(self, 
+        worker_id, data_points, dimensions, 
+        parallelism,clusters, max_iters,threshold):
+        self.worker_id = worker_id
+        self.num_dimensions = dimensions
+        self.num_clusters = clusters
+        self.max_iterations = max_iters
+        self.partition_points = int(data_points / parallelism)
+        self.parallelism = parallelism
+        self.start_partition = self.partition_points * self.worker_id
+        self.end_partition = self.partition_points * (worker_id + 1)
+
+        self.correct_centroids = None
+        self.local_partition = None
+        self.local_centroids = None
+        self.local_sizes = None
+        self.local_membership = None
+
+        self.global_delta = GlobalDelta( self.parallelism)
+        self.global_centroids = GlobalCentroids(self.num_clusters,
+                                                self.parallelism)
+        self.threshold = threshold
+
+    def run(self):
+        breakdown = []
+        breakdown.append(time.time())
+
+        self.load_dataset()
+        #print(self.local_partition)
+
+        self.local_membership = np.zeros([self.local_partition.shape[0]])
+
+        # barrier before starting iterations, to avoid different execution times
+        global barrier
+        barrier.wait()
+        init_time = time.time()
+        breakdown.append(init_time)
+        iter_count = 0
+        global_delta_val = 1
+        while ((iter_count < self.max_iterations) and 
+                (global_delta_val > self.threshold)):
+
+            # Get local copy of global objects
+            self.correct_centroids = self.global_centroids.get_centroids()
+            breakdown.append(time.time())
+
+            # Reset data structures that will be used in this iteration
+            self.local_sizes = np.zeros([self.num_clusters])
+            self.local_centroids = np.zeros([self.num_clusters, self.num_dimensions])
+
+            # Compute phase, returns number of local membership modifications
+            delta = self.compute_clusters()
+            breakdown.append(time.time())
+
+            # Update global objects
+            self.global_delta.update(delta, self.local_partition.shape[0])
+            self.global_centroids.update(self.local_centroids, self.local_sizes)
+
+            breakdown.append(time.time())
+            barrier.wait()
+            breakdown.append(time.time())
+            global_delta_val = self.global_delta.get_delta()
+            iter_count += 1
+
+        breakdown.append(time.time())
+        iteration_time = breakdown[-1] - init_time
+        return breakdown
+
+    def load_dataset(self):
+        import random
+        self.local_partition = np.random.randn(self.partition_points ,self.num_dimensions)
+        #self.local_partition = np.random.randn(self.end_partition -self.start_partition ,self.num_dimensions)
+
+    def distance(self, point, centroid):
+        """Euclidean squared distance."""
+        return np.linalg.norm(point - centroid)
+
+    def compute_clusters(self):
+        delta = 0
+        for i in range(0, self.end_partition-self.start_partition):
+            point = self.local_partition[i]
+            dists = ((point - self.correct_centroids) ** 2).sum(axis=1)
+            cluster = np.argmin(dists)
+
+            self.local_centroids[cluster] += point
+
+            self.local_sizes[cluster] += 1
+
+            # If now point is a member of a different cluster
+            if self.local_membership[i] != cluster:
+                delta += 1
+                self.local_membership[i] = cluster
+
+        return delta
+
+if __name__ == "__main__":
+    main(695866,100,25,6,10,0.00001, False, False)
